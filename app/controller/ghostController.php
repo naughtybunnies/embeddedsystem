a6aceb654de3c978df57180b8749a5f52a93e990
kenjitong <oitip_thaiphakdee@hotmail.com>
2018-04-18 16:05:47 +0700

    recent not use

--- app/controller/ghostController.php
+++ app/controller/ghostController.php
@@ -1,10 +1,9 @@
-//มะใช้งาบ//
 <?php
   require '../app/model/homeModel.php';
-  class recentController extends Controller{
+  class ghostController extends Controller{
       public static function get($param){
         require_once('../app/template/head.phtml');
-        require_once('../app/view/recent/body.phtml');
+        require_once('../app/view/ghost/body.phtml');
         require_once('../app/template/foot.phtml');
 
       }
